02c3d8074e5f121918495a06bcac77ac85370cbd
Dheeraj <aruri.dheeraj21@gmail.com>
2021-11-10 12:50:54 +0530

    List All Employees

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collection.Generic;
 
 namespace Pdemo
 {
@@ -8,6 +9,13 @@ namespace Pdemo
         {
             Employee employee = new Employee{Id=1001, Name="Ram", salary=98000};
             Console.WriteLine(employee);
+
+            List<Employee> employees = new list<Employee>{
+                new Employee{Id=1002, Name="Sai", salary=97000.00}
+                new Employee{Id=1003, Name="Gayathri", salary=98000.00}
+            };
+            foreach(Employee emp in employees)
+            Console.WriteLine(emp);
         }
     }
 }
